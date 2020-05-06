ffc05f1ad29b8c4493e4557da325ae714c1c9c01
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-06 14:02:49 -0500

    add progressBar behavior

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -15,22 +15,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonFalse: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let quiz = [ ["Four + Two is equal to Six", "True"],
-                 ["Five - Three is greater tha One", "True"],
-                 ["Three  + Eight is less than Ten", "False"]]
+    let quiz = [
+        Question(text: "A slug's blood is green.", answer: "True"),
+        Question(text: "Approximately one quarter of human bones are in the feet.", answer: "True"),
+        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+        Question(text: "It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+        Question(text: "You can lead a cow down stairs but not up stairs.", answer: "False"),
+        Question(text: "Google was originally called 'Backrub'.", answer: "True"),
+        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True")
+    ]
     var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelQuestion.text = quiz[questionNumber][0]
+        labelQuestion.text = quiz[questionNumber].text
+        progressBar.progress = Float(questionNumber + 1)/Float(quiz.count)
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         let userAnswer = sender.currentTitle
-        if (userAnswer == quiz[questionNumber][1]) {
-            print("Right")
+        if (userAnswer == quiz[questionNumber].answer) {
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Wrong")
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber < (quiz.count - 1) {
@@ -39,12 +51,15 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
+        Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
-        updateQuestion()
     }
     
-    private func updateQuestion() {
-        labelQuestion.text = quiz[questionNumber][0]
+    @objc func updateUI() {
+        labelQuestion.text = quiz[questionNumber].text
+        buttonTrue.backgroundColor = UIColor.clear
+        buttonFalse.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1)/Float(quiz.count)
     }
 }
 
