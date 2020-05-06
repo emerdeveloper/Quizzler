ddebcb3c7dffa55af14302cb918040ed125a044f
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-06 10:54:39 -0500

    change question when press buttons

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelQuestion: UILabel!
+    @IBOutlet weak var buttonTrue: UIButton!
+    @IBOutlet weak var buttonFalse: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [ ["Four + Two is equal to Six", "True"],
+                 ["Five - Three is greater tha One", "True"],
+                 ["Three  + Eight is less than Ten", "False"]]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        labelQuestion.text = quiz[questionNumber][0]
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        if (userAnswer == quiz[questionNumber][1]) {
+            print("Right")
+        } else {
+            print("Wrong")
+        }
+        
+        if questionNumber < (quiz.count - 1) {
+                questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        
+        updateQuestion()
+    }
+    
+    private func updateQuestion() {
+        labelQuestion.text = quiz[questionNumber][0]
+    }
 }
 
